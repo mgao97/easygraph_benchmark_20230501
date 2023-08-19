d0d63096de684e7bf61ce9e3ae9592388226d467
Min <Min@node0.c220g2.easygraph97-pg0.wisc.cloudlab.us>
2023-08-19 07:19:44 -0500

    add hp on rw with nx

--- profile_undirected_hp_rw_nx.py
+++ profile_undirected_hp_rw_nx.py
@@ -1,4 +1,4 @@
-# import igraph as ig 
+import igraph as ig 
 import easygraph as eg
 from benchmark import benchmark
 import random
@@ -23,6 +23,13 @@ def pre_data(path, file):
     np.savetxt(path+file, data, fmt='%i')
     return path+file
 
+def read_graph(g, filename):
+            with open(filename, "r") as f:
+                for line in f:
+                    # print(line)
+                    source, target = line.strip().split()  # Assuming edges are space-separated
+                    g.add_edge(source, target)
+            return g
 
 if __name__ == "__main__":
 
@@ -35,122 +42,128 @@ if __name__ == "__main__":
         if file == '.DS_Store':
             continue
         
-        # =======================EasyGraph=======================
-        filename = filepath + file    
-        print(f"Profiling dataset {file}")
+        # =======================NetworkX=======================
+        if file in ['ca-CondMat_lcc.txt', 'email-Enron_lcc.txt']:
+            filename = filepath + file    
+            print(f"Profiling dataset {file}")
 
-        print("Profileing loading")
-        print("=================")
-        print()
-        benchmark('nx.Graph().add_edges_from_file(filename)', globals=globals(), n=n)
-
-        g = nx.Graph()
-        g.add_edges_from_file(filename)
-
-        # node_num: sample node for dijkstra
-        node_num = 1000
-        start_idx, end_idx = 0, len(g.nodes)-1
-        random_node_index_list = random_nodes(node_num, start_idx, end_idx)
-        nodes = list(g.nodes)
-        nx_node_list = []
-
-        for index in random_node_index_list:
-            nx_node_list.append(nodes[index])
-
-        if "_lcc" not in file:
-            print("Profiling shortest path")
-            print("=======================")
+            print("Profileing loading")
+            print("=================")
             print()
-            benchmark('nx.shortest_path(g, sources = nx_node_list)', globals=globals(), n=n)
+            benchmark('read_graph(nx.Graph(), filename)', globals=globals(), n=n)
+
+            
+            g = read_graph(nx.Graph(), filename)
+
+            # # node_num: sample node for dijkstra
+            # node_num = 100
+            # start_idx, end_idx = 0, len(g.nodes)-1
+            # random_node_index_list = random_nodes(node_num, start_idx, end_idx)
+            # nodes = list(g.nodes)
+            # nx_node_list = []
+
+            # for index in random_node_index_list:
+            #     nx_node_list.append(nodes[index])
+
+            # # if "_lcc" not in file:
+            # print("Profiling shortest path")
+            # print("=======================")
+            # print()
+
+            # for node in nx_node_list:
+            #     benchmark('nx.shortest_path(g, source = node)', globals=globals(), n=n)
+
 
             print("========k-core=======")
             print("=================")
             print()
+            g.remove_edges_from(nx.selfloop_edges(g))
             benchmark('nx.core_number(g)', globals=globals(), n=n)
 
-            print("========betweenness_centrality=======")
+            # print("========betweenness_centrality=======")
+            # print("=================")
+            # print()
+            # benchmark('nx.betweenness_centrality(g)', globals=globals(), n=n)
+
+            # # since igraph only supports the metric of closeness centrality for lcc of each dataset, we choose the same for fair comparison
+            print("========closeness_centrality=======")
             print("=================")
             print()
-            benchmark('nx.betweenness_centrality(g)', globals=globals(), n=n)
-        # since igraph only supports the metric of closeness centrality for lcc of each dataset, we choose the same for fair comparison
-        print("========closeness_centrality=======")
-        print("=================")
-        print()
-        benchmark('nx.closeness_centrality(g)', globals=globals(), n=n)
+            benchmark('nx.closeness_centrality(g)', globals=globals(), n=n)
         
 
         
-        # print("Profiling loading")
-        # print("=================")
-        # print()
-        # benchmark('eg.GraphC().add_edges_from_file(filename, weighted=False,is_transform=True)', globals=globals(), n=n)
+            print("Profiling loading")
+            print("=================")
+            print()
+            benchmark('eg.GraphC().add_edges_from_file(filename, weighted=False,is_transform=True)', globals=globals(), n=n)
 
-        # g = eg.GraphC()
-        # g.add_edges_from_file(filename, weighted=False,is_transform=True)
+            g = eg.GraphC()
+            g.add_edges_from_file(filename, weighted=False,is_transform=True)
 
 
-        # # node_num: sample node for dijkstra
-        # node_num = 1000
-        # start_idx, end_idx = 0, len(g.nodes)-1
-        # random_node_index_list = random_nodes(node_num, start_idx, end_idx)
-        # nodes = list(g.nodes)
-        # eg_node_list = []
+            # node_num: sample node for dijkstra
+            node_num = 100
+            start_idx, end_idx = 0, len(g.nodes)-1
+            random_node_index_list = random_nodes(node_num, start_idx, end_idx)
+            nodes = list(g.nodes)
+            eg_node_list = []
 
-        # for index in random_node_index_list:
-        #     eg_node_list.append(nodes[index])
+            for index in random_node_index_list:
+                eg_node_list.append(nodes[index])
 
-        # if "_lcc" not in file:
-        #     print("Profiling shortest path")
-        #     print("=======================")
-        #     print()
-        #     benchmark('eg.multi_source_dijkstra(g, sources = eg_node_list)', globals=globals(), n=n)
+            if "_lcc" not in file:
+                print("Profiling shortest path")
+                print("=======================")
+                print()
+                benchmark('eg.multi_source_dijkstra(g, sources = eg_node_list)', globals=globals(), n=n)
 
-        #     print("========k-core=======")
-        #     print("=================")
-        #     print()
-        #     benchmark('eg.k_core(g)', globals=globals(), n=n)
+                print("========k-core=======")
+                print("=================")
+                print()
+                benchmark('eg.k_core(g)', globals=globals(), n=n)
 
-        #     print("========betweenness_centrality=======")
-        #     print("=================")
-        #     print()
-        #     benchmark('eg.betweenness_centrality(g)', globals=globals(), n=n)
-        # # since igraph only supports the metric of closeness centrality for lcc of each dataset, we choose the same for fair comparison
-        # print("========closeness_centrality=======")
-        # print("=================")
-        # print()
-        # benchmark('eg.closeness_centrality(g)', globals=globals(), n=n)
+            #     print("========betweenness_centrality=======")
+            #     print("=================")
+            #     print()
+            #     benchmark('eg.betweenness_centrality(g)', globals=globals(), n=n)
+            # # since igraph only supports the metric of closeness centrality for lcc of each dataset, we choose the same for fair comparison
+            print("========closeness_centrality=======")
+            print("=================")
+            print()
+            benchmark('eg.closeness_centrality(g)', globals=globals(), n=n)
         
 
 
-        # # =======================igraph=======================
-        # print(f"Profiling dataset {file}")
+            # =======================igraph=======================
+            print(f"Profiling dataset {file}")
 
-        # print("Profiling loading")
-        # print("=================")
-        # print()
+            print("Profiling loading")
+            print("=================")
+            print()
+                
+            benchmark("ig.Graph.Read_Edgelist(filename,False)", globals=globals(), n=n)
+            g= ig.Graph.Read_Edgelist(filename,False)
             
-        # benchmark("ig.Graph.Read_Edgelist(filename,False)", globals=globals(), n=n)
-        # g= ig.Graph.Read_Edgelist(filename,False)
-        
 
 
-        # ig_node_list = [int(i) for i in eg_node_list]    
-        # if "_lcc" not in file:
-        #     print("Profiling shortest path")
-        #     print("=======================")
-        #     print()
-        #     benchmark("g.distances(source = ig_node_list,weights=[1]*len(g.es))", globals=globals(), n=n)
-        #     print("========k-core=======")
-        #     print("=================")
-        #     print()
-        #     benchmark('g.coreness()', globals=globals(), n=n)
-        #     print("========betweenness_centrality=======")
-        #     print("=================")
-        #     print()
-        #     benchmark('g.betweenness(directed=False,weights=[1]*len(g.es))', globals=globals(), n=n)
-        # # We only measure closeness for lcc of each dataset
-        # print("========closeness_centrality=======")
-        # print("=================")
-        # print()
-        # benchmark('g.closeness(weights=[1]*len(g.es))', globals=globals(), n=n)
+            ig_node_list = [int(i) for i in eg_node_list]    
+            if "_lcc" not in file:
+                print("Profiling shortest path")
+                print("=======================")
+                print()
+                benchmark("g.distances(source = ig_node_list,weights=[1]*len(g.es))", globals=globals(), n=n)
+                print("========k-core=======")
+                print("=================")
+                print()
+                benchmark('g.coreness()', globals=globals(), n=n)
+            #     print("========betweenness_centrality=======")
+            #     print("=================")
+            #     print()
+            #     benchmark('g.betweenness(directed=False,weights=[1]*len(g.es))', globals=globals(), n=n)
+            # # We only measure closeness for lcc of each dataset
+            print("========closeness_centrality=======")
+            print("=================")
+            print()
+            benchmark('g.closeness(weights=[1]*len(g.es))', globals=globals(), n=n)
     
